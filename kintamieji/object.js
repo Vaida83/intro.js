7866bc6a044d4aac89219e848a48a1cc9d535939
Vaida <b.vaida.b@gmail.com>
2023-09-25 22:40:53 +0300

    object

---
+++ kintamieji/object.js
@@ -0,0 +1,42 @@
+/*
+OBJECT - objektas
+*/
+
+console.clear();
+
+//vartotojas: vardas, amzius, ar vedes
+
+//const jonasName = 'Jonas';//
+//const jonasAge = '99';//
+//const jonasIsMarried = 'true';//
+
+//const maryteName = 'Maryte';//
+//const maryteAge = '88';//
+//const maryteIsMarried = 'false';//
+
+const jonasData = ['Jonas', 99, true];
+const maryteData = ['Maryte', 88, false];
+
+const jonasName = jonasData[0];
+const jonasAge = jonasData[1];
+const jonasIsMarried = jonasData[2];
+
+const maryteName = maryteData[0];
+const maryteAge = maryteData[1];
+const maryteIsMarried = maryteData[2];
+
+console.log(jonasName, jonasAge, jonasIsMarried);
+console.log(maryteName, maryteAge, maryteIsMarried);
+
+//key: value
+
+const jonas = {
+    name: "Jonas",
+    age: 99,
+   isMarried: true,
+};
+
+console.log(jonas);
+console.log(jonas['name']);
+console.log(jonas['age']);
+console.log(jonas['isMarried']);
